254a46a019148c95b203788fcea08962c6022bab
Kejdi <kejdimuci.dev@gmail.com>
2025-08-18 20:33:04 +0200

    Add Fruits_in_baskets_ii.java solution

---
+++ easy/Fruits_in_baskets_ii.java
@@ -0,0 +1,23 @@
+public class Fruits_in_baskets_ii {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[n];
+        int unplaced = 0;
+
+        for (int fruit : fruits) {
+            boolean placed = false;
+
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruit) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }
+}
